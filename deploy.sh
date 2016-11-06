e563a637bbda9667a2314980467af4ef9b9ca534
Milan Aleksic <milanaleksic@gmail.com>
2016-11-06 10:48:32 +0100

    explicit content type setting for PDFs

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,15 @@ function aws() {
 }
 
 # Copy over pages - not static js/img/css/downloads
-aws s3 --region $REGION sync --acl "public-read" --sse "AES256" /data/public/ s3://$BUCKET_NAME --exclude 'post'
+aws s3 --region $REGION sync --acl "public-read" --sse "AES256" /data/public/ s3://$BUCKET_NAME --exclude 'public/cv*'
+
+# backwards-compatible links don't have .pdf extensions, so upload must happen manually
+aws s3 cp /data/public/public/cv s3://milanaleksic.net-cdn/public/ --content-type 'application/pdf'
+aws s3 cp /data/public/public/cv-app1 s3://milanaleksic.net-cdn/public/ --content-type 'application/pdf'
+aws s3 cp /data/public/public/cv-app2 s3://milanaleksic.net-cdn/public/ --content-type 'application/pdf'
+aws s3 cp /data/public/public/cv-app3 s3://milanaleksic.net-cdn/public/ --content-type 'application/pdf'
+aws s3 cp /data/public/public/cv-app4 s3://milanaleksic.net-cdn/public/ --content-type 'application/pdf'
+aws s3 cp /data/public/public/cv-nostrification s3://milanaleksic.net-cdn/public/ --content-type 'image/jpeg'
 
 # Invalidate root page and page listings
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths '/*'
